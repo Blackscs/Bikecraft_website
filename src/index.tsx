1fd3c2c0e8388eaaa0405e710c28b87a1c1cdb86
Mario Santos <marioosantos238412@gmail.com>
2024-09-14 16:23:54 -0300

    Final version 6

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 /* @refresh reload */
 import { render } from "solid-js/web";
 
-import "../src/styles/styles.css";
+import "styles/styles.css";
 import App from "./App.tsx";
 
 render(() => <App/>, document.getElementById("root") as HTMLElement);
